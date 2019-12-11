8fc67d8f0b26ed4792ea5194c3610fbf5e1740cd
AntoineLer <antoine.leroy@hotmail.com>
2019-12-11 09:51:02 +0100

    75%

--- nBits.py
+++ nBits.py
@@ -34,8 +34,8 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.decomposition import PCA, KernelPCA
 from sklearn.preprocessing import StandardScaler
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import cross_val_score, cross_validate
 from sklearn.metrics import roc_auc_score
 from sklearn.feature_selection import SelectFromModel
@@ -45,12 +45,13 @@ from imblearn.over_sampling import ADASYN, SMOTE, RandomOverSampler
 from imblearn.under_sampling import TomekLinks, RandomUnderSampler, EditedNearestNeighbours, CondensedNearestNeighbour
 from imblearn.combine import SMOTETomek
 from imblearn.ensemble import BalancedRandomForestClassifier, BalancedBaggingClassifier, EasyEnsembleClassifier
-from imblearn.pipeline import make_pipeline
+from imblearn.pipeline import make_pipeline, Pipeline
 
 
 def get_RF_model(class_weight=None):
     return RandomForestClassifier(oob_score=True, n_jobs=-1, random_state=50, min_samples_leaf=1, bootstrap=True,
                                   max_features="log2", n_estimators=1000, max_depth=None, criterion='gini', max_leaf_nodes=None, class_weight=class_weight)
+
 
 @contextmanager
 def measure_time(label):
@@ -94,8 +95,6 @@ def load_from_csv(path, delimiter=','):
     return pd.read_csv(path, delimiter=delimiter)
 
 
-
-
 def create_fingerprints(chemical_compounds, nBits=124):
     """
     Create a learning matrix `X` with (Morgan) fingerprints
@@ -120,7 +119,7 @@ def create_fingerprints(chemical_compounds, nBits=124):
 
     for i in range(n_chem):
         m = Chem.MolFromSmiles(chemical_compounds[i])
-        X[i,:] = AllChem.GetMorganFingerprintAsBitVect(m,2,nBits=nBits)
+        X[i, :] = AllChem.GetMorganFingerprintAsBitVect(m, 2, nBits=nBits)
 
     return X
 
@@ -162,9 +161,9 @@ def make_submission(y_predicted, auc_predicted, file_name="submission", date=Tru
     # Writing into the file
     with open(file_name, 'w') as handle:
         handle.write('"Chem_ID","Prediction"\n')
-        handle.write('Chem_{:d},{}\n'.format(0,auc_predicted))
+        handle.write('Chem_{:d},{}\n'.format(0, auc_predicted))
 
-        for n,idx in enumerate(indexes):
+        for n, idx in enumerate(indexes):
 
             if np.isnan(y_predicted[n]):
                 raise ValueError('The prediction cannot be NaN')
@@ -172,19 +171,21 @@ def make_submission(y_predicted, auc_predicted, file_name="submission", date=Tru
             handle.write(line)
     return file_name
 
-def plot_2d_space(X, y, label='Classes'): 
-    plt.figure()  
+
+def plot_2d_space(X, y, label='Classes'):
+    plt.figure()
     colors = ['#1F77B4', '#FF7F0E']
     markers = ['o', 's']
     for l, c, m in zip(np.unique(y), colors, markers):
         plt.scatter(
-            X[y==l, 0],
-            X[y==l, 1],
+            X[y == l, 0],
+            X[y == l, 1],
             c=c, label=l, marker=m
         )
     plt.title(label)
     plt.legend(loc='upper right')
     plt.savefig("PCA.pdf")
+
 
 def display_confusion_matrix(model, X_train, y_train, X_test, y_test, save="confusion_matrix.pdf", title="Confusion Matrix"):
     model.fit(X_train, y_train)
@@ -221,21 +222,140 @@ def get_info_features(y_LS, LS, save="Count.pdf"):
 
 def do_sampling_research(X_train, y_train, X_test, y_test, nBits=124, info_features=False):
 
-    print("First tests without sampling:")
+    print("First tests:")
     # Without sampling
     if info_features:
-        get_info_features(y_train, pd.DataFrame({'ACTIVE': y_train}), save="Count_before_{}.pdf".format(nBits))
-    pipeline = make_pipeline(ADASYN(sampling_strategy=0.25, random_state=64), BalancedRandomForestClassifier(n_estimators=1000, random_state=18, n_jobs=-1))
+        get_info_features(y_train, pd.DataFrame(
+            {'ACTIVE': y_train}), save="Count_before_{}.pdf".format(nBits))
+    pipeline = make_pipeline(ADASYN(sampling_strategy=0.25, random_state=64, n_jobs=-1),
+                             BalancedRandomForestClassifier(n_estimators=100, random_state=18, n_jobs=-1))
     display_confusion_matrix(pipeline, X_train, y_train, X_test, y_test,
                              save="confusion_matrix_before_{}.pdf".format(nBits), title="Confusion Matrix before sampling with {} nBits".format(nBits))
 
-    print("\nSecond test with sampling:")
+    print("\nSecond test:")
     # With sampling
     if info_features:
-        get_info_features(y_train, pd.DataFrame({'ACTIVE': y_train}), save="Count_after_{}.pdf".format(nBits))
-    pipeline = make_pipeline(ADASYN(sampling_strategy=0.35, random_state=64), BalancedRandomForestClassifier(n_estimators=1000, random_state=18, n_jobs=-1))
+        get_info_features(y_train, pd.DataFrame(
+            {'ACTIVE': y_train}), save="Count_after_{}.pdf".format(nBits))
+    pipeline = make_pipeline(ADASYN(sampling_strategy=0.28, random_state=64, n_jobs=-1),
+                             BalancedRandomForestClassifier(n_estimators=50, random_state=18, n_jobs=-1))
     display_confusion_matrix(pipeline, X_train, y_train, X_test, y_test,
                              save="confusion_matrix_after_{}.pdf".format(nBits), title="Confusion Matrix after sampling with {} nBits".format(nBits))
+
+
+def do_research(LS):
+    nBits = [124, 1250, 1800]
+    info_features = True
+    for nbits in nBits:
+        with measure_time("Creating fingerprint"):
+            X_LS = create_fingerprints(LS["SMILES"].values, nBits=nbits)
+            # drop duplicate
+            data = pd.DataFrame(X_LS)
+            data = data.drop_duplicates()
+            X_LS = data.values
+
+        # Drop also duplicate in the y_LS samples
+        y_LS = LS["ACTIVE"].loc[data.index].values
+        X_train, X_test, y_train, y_test = train_test_split(
+            X_LS, y_LS, test_size=0.25, train_size=0.75, random_state=1)
+        print(X_train.shape, X_test.shape)
+        print("nBits =", nbits)
+        do_sampling_research(X_train, y_train, X_test,
+                             y_test, info_features=info_features, nBits=nbits)
+        info_features = False
+        pca = None
+
+
+def do_CV_RF(LS, cv=10):
+    nBits = 1250
+    with measure_time("Creating fingerprint"):
+        X_LS = create_fingerprints(LS["SMILES"].values, nBits=nBits)
+        # drop duplicate
+        data = pd.DataFrame(X_LS)
+        data = data.drop_duplicates()
+        X_LS = data.values
+
+    # Drop also duplicate in the y_LS samples
+    y_LS = LS["ACTIVE"].loc[data.index].values
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_LS, y_LS, test_size=0.25, train_size=0.75, random_state=1)
+    pipeline = make_pipeline(ADASYN(sampling_strategy=0.25, random_state=64, n_jobs=-1),
+                             BalancedRandomForestClassifier(n_estimators=800, random_state=18, n_jobs=-1))
+    scores = cross_validate(pipeline, X_train, y_train, cv=cv, scoring=(
+        'roc_auc', 'average_precision'), return_estimator=True)
+    print(scores['test_roc_auc'].mean(),
+          scores['test_average_precision'].mean())
+    model = scores['estimator'][np.argmax(scores['test_roc_auc'])]
+    y_pred = model.predict(X_test)
+    conf_mat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+    print("confusion_matrix:\n", conf_mat)
+
+
+def do_CV_Voting(LS, cv=10):
+    nBits = 1250
+    with measure_time("Creating fingerprint"):
+        X_LS = create_fingerprints(LS["SMILES"].values, nBits=nBits)
+        # drop duplicate
+        data = pd.DataFrame(X_LS)
+        data = data.drop_duplicates()
+        X_LS = data.values
+
+    # Drop also duplicate in the y_LS samples
+    y_LS = LS["ACTIVE"].loc[data.index].values
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_LS, y_LS, test_size=0.25, train_size=0.75, random_state=1)
+    pipeline_1 = make_pipeline(ADASYN(sampling_strategy=0.25, random_state=64, n_jobs=-1),
+                               BalancedRandomForestClassifier(n_estimators=600, random_state=18, n_jobs=-1))
+    pipeline_2 = make_pipeline(ADASYN(random_state=64, n_jobs=-1),
+                               BalancedRandomForestClassifier(n_estimators=600, random_state=24, n_jobs=-1))
+    BRF = BalancedRandomForestClassifier(n_estimators=100, random_state=18, n_jobs=-1)
+    BGC = make_pipeline(ADASYN(sampling_strategy=0.25, random_state=64, n_jobs=-1),
+                        BalancedBaggingClassifier(estimator=DecisionTreeClassifier(max_features="log2"), n_estimators=50))
+    votingModel = VotingClassifier(estimators=[(
+        'pip1', pipeline_1), ('pip2', pipeline_2), ('BRF', BRF), ('BGC', BGC)], voting='soft', weights=[3, 1, 1, 1], n_jobs=-1)
+    scores = cross_validate(votingModel, X_train, y_train, cv=cv, scoring=(
+        'roc_auc', 'average_precision'), return_estimator=True)
+    print(scores['test_roc_auc'].mean(),
+          scores['test_average_precision'].mean())
+    model = scores['estimator'][np.argmax(scores['test_roc_auc'])]
+    y_pred = model.predict(X_test)
+    conf_mat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+    print("confusion_matrix:\n", conf_mat)
+
+
+def do_CV_grid(LS, cv=10):
+    nBits = 1250
+    with measure_time("Creating fingerprint"):
+        X_LS = create_fingerprints(LS["SMILES"].values, nBits=nBits)
+        # drop duplicate
+        data = pd.DataFrame(X_LS)
+        data = data.drop_duplicates()
+        X_LS = data.values
+    # Drop also duplicate in the y_LS samples
+    y_LS = LS["ACTIVE"].loc[data.index].values
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_LS, y_LS, test_size=0.25, train_size=0.75, random_state=1)
+    pipeline = Pipeline([('ada', ADASYN(sampling_strategy=0.25, random_state=64, n_jobs=-1)),
+                         ('BRF', BalancedRandomForestClassifier(n_estimators=500, random_state=18, n_jobs=-1, bootstrap=False))])
+    param = {}
+    param['BRF__n_estimators'] = [500]
+    param['BRF__max_features'] = [None, 'log2']
+    #param['BRF__criterion'] = ['gini', 'entropy']
+
+    clf = GridSearchCV(pipeline, param, scoring='roc_auc', n_jobs=2, cv=10)
+    clf.fit(X_train, y_train)
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.cv_results_)
+    print(clf.best_params_)
+    print(clf.best_score_)
+    print()
+
+    y_pred = clf.predict(X_test)
+    conf_mat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+    print("confusion_matrix:\n", conf_mat)
+    print("Classification report")
+    print(classification_report(y_true=y_test, y_pred=y_pred))
 
 
 if __name__ == '__main__':
@@ -256,66 +376,39 @@ if __name__ == '__main__':
 
     # LEARNING
     # Create fingerprint features and output
-    nBits = [124, 250, 500, 1000, 1250, 1500, 1800]
-    info_features = True
-    for nbits in nBits:
-        with measure_time("Creating fingerprint"):
-            X_LS = create_fingerprints(LS["SMILES"].values, nBits=nbits)
-            # drop duplicate
-            data = pd.DataFrame(X_LS)
-            data = data.drop_duplicates()
-            X_LS = data.values
-
-        # Drop also duplicate in the y_LS samples
-        y_LS = LS["ACTIVE"].loc[data.index].values
-        X_train, X_test, y_train, y_test = train_test_split(X_LS, y_LS, test_size=0.25, train_size=0.75, random_state=1)
-        """
-        select = SelectFromModel(estimator=BalancedRandomForestClassifier(n_estimators=500), threshold='mean')
-        select = select.fit(X_train, y_train)
-        X_train = select.transform(X_train)
-        X_test = select.transform(X_test)
-        """
-        print(X_train.shape, X_test.shape)
-
-        """
-        data = pd.DataFrame(X_train)
-        data['Target'] = y_train
-        inactive_index = data[data['Target'] == 0].index
-        length = len(inactive_index)
-        drop_indices = np.random.choice(inactive_index, round(0.66*length), replace=False)
-        data = data.drop(drop_indices)
-        y_train = data.Target.values
-        data = data.drop("Target", axis=1)
-        X_train = data.values
-        """
-        print("nBits =",nbits)
-        do_sampling_research(X_train, y_train, X_test, y_test, info_features=info_features, nBits=nbits)
-        info_features = False
-        pca = None
-
+    # do_research(LS)
+    #do_CV_RF(LS, cv=10)
+    #do_CV_test(LS, cv=10)
+    #do_CV_Voting(LS, cv=10)
+    do_CV_grid(LS, cv=10)
     exit("Preprocessing research end")
+    nBits = 1250
+    with measure_time("Creating fingerprint"):
+        X_LS = create_fingerprints(LS["SMILES"].values, nBits=nBits)
+        # drop duplicate
+        data = pd.DataFrame(X_LS)
+        data = data.drop_duplicates()
+        X_LS = data.values
+
+    # Drop also duplicate in the y_LS samples
+    y_LS = LS["ACTIVE"].loc[data.index].values
     # Build the model
-    model = RandomForestClassifier()
+    model = make_pipeline(ADASYN(sampling_strategy=0.25, random_state=64, n_jobs=-1),
+                          BalancedRandomForestClassifier(n_estimators=2500, random_state=18, n_jobs=-1))
 
     with measure_time('Training'):
         model.fit(X_LS, y_LS)
 
     # PREDICTION
     TS = load_from_csv(args.ts)
-    X_TS = create_fingerprints(TS["SMILES"].values)
+    X_TS = create_fingerprints(TS["SMILES"].values, nBits=nBits)
 
     # Predict
-    y_pred = model.predict_proba(X_TS)[:,1]
+    y_pred = model.predict_proba(X_TS)[:, 1]
 
     # Estimated AUC of the model
-    auc_predicted = 0.7 # it seems a bit pessimistic, right?
+    auc_predicted = 0.7  # it seems a bit pessimistic, right?
 
     # Making the submission file
     fname = make_submission(y_pred, auc_predicted, 'toy_submission_DT')
     print('Submission file "{}" successfully written'.format(fname))
-
-
-
-
-
-
